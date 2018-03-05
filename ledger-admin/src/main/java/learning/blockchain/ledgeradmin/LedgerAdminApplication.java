4b6e1bf7a3511d210298726f94f6d343f5b8d5df
wangzhenpeng <1228916292@qq.com>
2018-03-05 22:00:11 +0800

    first commit

---
+++ ledger-admin/src/main/java/learning/blockchain/ledgeradmin/LedgerAdminApplication.java
@@ -0,0 +1,12 @@
+package learning.blockchain.ledgeradmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LedgerAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LedgerAdminApplication.class, args);
+	}
+}
