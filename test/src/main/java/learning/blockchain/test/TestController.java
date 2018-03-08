5e5fe8a6006b01ad884f35a55d9f13739ecb73c8
wangzhenpeng <1228916292@qq.com>
2018-03-08 18:25:28 +0800

    auto config

---
+++ test/src/main/java/learning/blockchain/test/TestController.java
@@ -0,0 +1,42 @@
+package learning.blockchain.test;
+
+import learning.blockchain.configuration.LedgerOrgs;
+import org.hyperledger.fabric.sdk.HFClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Create with IntelliJ IDEA
+ * Author               : wangzhenpeng
+ * Date                 : 2018/3/8
+ * Time                 : 下午12:17
+ * Description          :
+ */
+@RestController
+public class TestController {
+//    final HFClient hfClient;
+//
+//    @Autowired
+//    public TestController(HFClient hfClient) {
+//        this.hfClient = hfClient;
+//    }
+
+    final HFClient hfClient;
+    final LedgerOrgs ledgerOrgs;
+
+    @Autowired
+    public TestController(HFClient hfClient, LedgerOrgs ledgerOrgs) {
+        this.hfClient = hfClient;
+        this.ledgerOrgs = ledgerOrgs;
+    }
+
+
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    public String getName() {
+        System.out.println("test");
+        return hfClient.toString();
+    }
+
+}
