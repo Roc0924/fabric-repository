a25bea5578df3dc73ff78cc3d915d692115b85b7
wangzhenpeng <1228916292@qq.com>
2018-03-12 16:03:59 +0800

    delete

--- ledger-starter-test/src/main/java/learning/blockchain/test/controllers/TestController.java
+++ ledger-starter-test/src/main/java/learning/blockchain/test/controllers/TestController.java
@@ -3,12 +3,10 @@ package learning.blockchain.test.controllers;
 import learning.blockchain.configuration.LedgerOrgs;
 import learning.blockchain.test.dtos.ChainCodeDTO;
 import learning.blockchain.test.services.ChainCodeService;
-import learning.blockchain.test.services.UserService;
 import org.hyperledger.fabric.sdk.HFClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -23,17 +21,14 @@ public class TestController {
 
     final HFClient hfClient;
     final LedgerOrgs ledgerOrgs;
-    final UserService userService;
     final ChainCodeService chainCodeService;
 
     @Autowired
     public TestController(HFClient hfClient,
                           LedgerOrgs ledgerOrgs,
-                          UserService userService,
                           ChainCodeService chainCodeService) {
         this.hfClient = hfClient;
         this.ledgerOrgs = ledgerOrgs;
-        this.userService = userService;
         this.chainCodeService = chainCodeService;
     }
 
@@ -42,13 +37,6 @@ public class TestController {
     public String getName() {
         System.out.println("test");
         return hfClient.toString();
-    }
-
-    @RequestMapping(value = "/login", method = RequestMethod.GET)
-    public Boolean login(@RequestParam(name = "orgName") String orgName,
-                         @RequestParam(name = "userName") String userName,
-                         @RequestParam(name = "secret") String secret) {
-        return userService.login(orgName, userName, secret);
     }
 
     @RequestMapping(value = "/install", method = RequestMethod.GET)
