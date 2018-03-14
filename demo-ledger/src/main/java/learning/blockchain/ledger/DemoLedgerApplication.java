84e451d3217f28aa84c8914ade3455a32a747e96
wangzhenpeng <1228916292@qq.com>
2018-03-14 14:28:44 +0800

    first commit

---
+++ demo-ledger/src/main/java/learning/blockchain/ledger/DemoLedgerApplication.java
@@ -0,0 +1,12 @@
+package learning.blockchain.ledger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoLedgerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoLedgerApplication.class, args);
+	}
+}
